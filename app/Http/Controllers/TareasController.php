700e38e72269d7fcc671f49d9f28531c8ba2f170
DennisCalderon <dennis.tacna@gmail.com>
2020-04-11 17:44:48 -0500

    Creando controladores
    Se uso los comandos
     - php artisan make:controller TareasController
       comando para crear un controlador en blanco
     - php artisan make:controller EstudianteController --resource
       comando para crear un controlador con un CRUD completo

---
+++ app/Http/Controllers/TareasController.php
@@ -0,0 +1,13 @@
+<?php
+
+#php artisan make:controller TareasController   
+/* comando para crear un controlador en blanco */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TareasController extends Controller
+{
+    //
+}
