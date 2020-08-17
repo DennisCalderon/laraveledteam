3b4edefbbcbd2413112a5bf6a8ba03fd096a26f7
DennisCalderon <dennis.tacna@gmail.com>
2020-08-17 22:04:48 +0000

    Eloquetn ORM Avanzado - Repositories 	- Capa intermedia entre el controlador y el modelo, nos ayuda a trabajar los datos que obtenemos del modelo, contiene toda la lógica de acceso de los datos para poder crear el modelo. 	- Se utilizan cuando nuestra aplicación requiere compartir muchisimas consultas de Eloquent dentro de nuestra lógica de negocio. 	- Con esto el repositorio se encargue de hacer las consultas(repetidas constantemente) y luego de obtener los datos se muestra el resultado a través de un controlador. 	- Se recomienda crear una carpeta adicional llamda "Repositories" en: "app\Repositories" y luego se crea un archivo referencial como: "TareasRepository.php" 	- Este archivo nos va permitir trabajar con los datos a través del modelo y a través del controlador, para que esa información posteriormente la podamos reutilizar y se nos haga mas sencillo el uso de las consultas repetitivas, ej: carrito de compras, compras al mes, listado del carrito de compras, otros.

--- app/Http/Controllers/TareasController.php
+++ app/Http/Controllers/TareasController.php
@@ -5,20 +5,29 @@
 
 namespace App\Http\Controllers;
 use App\Entities\Tarea;
+use App\Repositories\TareasRepository;
 
 use Illuminate\Http\Request;
 
 class TareasController extends Controller
 {
+    public function __construct(TareasRepository $tareas) {
+        $this->tareas = $tareas;
+    }
+
     public function index() {
         #pruebas
         /*$tareas = Tarea::all();
         var_dump($tareas);*/
 
-        $tareas = Tarea::where('usuario_id', 1)->get();
+        //$tareas = Tarea::where('usuario_id', 1)->get();
+        #$tareasPrioridad = $this->tareas->porPrioridad(1,3);
+        $tareas = $this->tareas->porUsuario(1);
         foreach ($tareas as $tarea) {
             echo $tarea->titulo."<br />";
         }
+        
+        
     }
     public function buscar() {
         echo "Buscar en Tareas Controller";
---
+++ app/Repositories/TareasRepository.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Repositories;
+use App\Entities\Tarea;
+
+class TareasRepository {
+    public function porUsuario($id) {
+        return Tarea::where('usuario_id', $id)->orderBy('created_at', 'asc')->get();
+    }
+    public function porPrioridad($id, $prioridad_id) {
+        return Tarea::where('usuario_id', $id)
+                            ->where('prioridad_id', $prioridad_id)
+                            ->orderBy('created_at', 'asc')->get();
+    }
+}
+
+?>
